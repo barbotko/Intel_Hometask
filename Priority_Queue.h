3023594030d3c9e34c258c320fc58610db8c4d67
barbotko <lolwhat17@mail.ru>
2013-04-11 16:55:59 +0400

    Update

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -12,7 +12,8 @@ class priority_queue
 private:
     queue_elem heap[QUEUE_SIZE];                   // contains the queue
     int heap_size;                                 // contains the number of elements
-    void Heapify();                                // to put elements in the correct order
+    void Heapify(int start_point);                 // to put elements in the correct order
+    void Swap(queue_elem& el1, queue_elem& el2);   // to swap two elements in the heap
 public:
     void Queue_Insert(Data value, int priority);   // to add element
     void Extract_Elem();                           // removes the elements with the highest prioiry from queue
@@ -37,3 +38,24 @@ priority_queue::~priority_queue()
 
 }
 
+void priority_queue::Heapify(int start_point)
+{
+    int largest = start_point;
+    int left  = 2 * start_point + 1;
+    int right = 2 * start_point + 2;
+    if (left <= heap_size && heap[left].priority >= heap[largest].priority)
+        largest = left;
+    else if (right <= heap_size && heap[right].priority >= heap[largest].priority)
+        largest = right;
+    if (largest != start_point) swap(heap[largest], heap[start_point])
+    Heapify(largest);
+    return;
+}
+
+void Swap(queue_elem& el1, queue_elem& el2)
+{
+    queue_elem exch = el1;
+    el1 = el2;
+    el2 = exch;
+    return;
+}
