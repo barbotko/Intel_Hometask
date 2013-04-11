2c0d70c36555f33f5ab4b3fc54696501f60aad4c
barbotko <lolwhat17@mail.ru>
2013-04-11 20:57:41 +0400

    Update

--- Priority_Queue.h
+++ Priority_Queue.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <assert.h>
+
 const int QUEUE_SIZE = 100;
 typedef int Data;
 
@@ -16,8 +19,8 @@ private:
     void Swap(queue_elem& el1, queue_elem& el2);   // to swap two elements in the heap
 public:
     void Queue_Insert(Data value, int priority);   // to add element
-    void Extract_Elem();                           // removes the elements with the highest prioiry from queue
-    bool Queue_Is_Empty();                         // checks if the queue is empty
+    queue_elem Extract_Elem();                     // removes the elements with the highest prioiry from queue
+    bool Queue_Is_Not_Empty();                     // checks if the queue is empty
     void Queue_Dump();                             // to print queue's contents
     priority_queue();
     ~priority_queue();
@@ -27,15 +30,26 @@ priority_queue::priority_queue()
 {
     heap_size = 0;
     for (int i = 0; i < QUEUE_SIZE; ++i)
-    {
-        heap[i].priority = 0;
-        heap[i].value = 0;
-    }
+        heap[i] = {0, 0};
 }
 
 priority_queue::~priority_queue()
 {
 
+}
+
+queue_elem priority_queue::Extract_Elem()
+{
+    assert( Queue_Is_Not_Empty() );
+    queue_elem max = heap[0];
+    heap[0] = heap[heap_size--];
+    Heapify(0);
+    return max;
+}
+
+bool priority_queue::Queue_Is_Not_Empty()
+{
+    if (heap_size == 0) return false; else return true;
 }
 
 void priority_queue::Heapify(int start_point)
@@ -47,7 +61,7 @@ void priority_queue::Heapify(int start_point)
         largest = left;
     else if (right <= heap_size && heap[right].priority >= heap[largest].priority)
         largest = right;
-    if (largest != start_point) Swap(heap[largest], heap[start_point])
+    if (largest != start_point) Swap(heap[largest], heap[start_point]);
     Heapify(largest);
     return;
 }
