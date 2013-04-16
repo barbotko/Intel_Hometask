2b060bbc3341ba38d07989702d47c7bc86efc2c2
barbotko <lolwhat17@mail.ru>
2013-04-17 00:28:53 +0400

    cpp files

--- Queue.cpp
+++ Queue.cpp
@@ -1,22 +1,3 @@
-#include "Priority_Queue.h"
-
-int main()
-{
-    priority_queue qu(5);
-    qu.Insert(1, -4);
-    qu.Dump();
-    qu.Insert(6, -2);
-    qu.Dump();
-    qu.Insert(5, -1);
-    qu.Dump();
-    qu.Insert(10, -3);
-    qu.Dump();
-    qu.Extract_Elem();
-    qu.Dump();
-    return 0;
-}
-
-
 //---------------------------------------------------------------
 //                  PUBLIC METHODS
 //---------------------------------------------------------------
